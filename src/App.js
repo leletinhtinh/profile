e8dd6587c56fd6ee398b28c95b0a0077c7108858
leletinhtinh <lvtinh.21@gmail.com>
2024-11-07 15:06:54 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '/components/Header';
+import Footer from '/components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <p>This is the projects page.</p>
+    </div>
+  );
+}
+
+export default Projects;
